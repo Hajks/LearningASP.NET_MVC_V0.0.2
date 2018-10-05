f6a99667a0e4c77d702ed1587dd100e8963cc332
Hajk <tunczorx@gmail.com>
2018-10-05 12:14:17 +0200

    Basics in ASP.NET MVC

--- LearningASP.NET_MVC_V0.0.2/Controllers/HomeController.cs
+++ LearningASP.NET_MVC_V0.0.2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LearningASP.NET_MVC_V0._0._2.Models;
 
 namespace LearningASP.NET_MVC_V0._0._2.Controllers
 {
@@ -10,12 +11,24 @@ namespace LearningASP.NET_MVC_V0._0._2.Controllers
     {
         public ActionResult Index()
         {
+            var albums = new List<Album> //Creating list of albums, generally we should insert here informations from database. 
+            {
+                new Album {Artist = "U2", AlbumName = "The Best of"},
+                new Album {Artist = "Coldplay", AlbumName = "Mylo Xyloto"}
+            };
+
+            ViewBag.Albums = albums;
             return View();
         }
 
+        //public string Index(string id)
+        //{
+        //    return "Witaj, " + id;
+        //}
+
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page."; //For example this information will be read by About.cshtml(Views)
 
             return View();
         }
---
+++ LearningASP.NET_MVC_V0.0.2/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearningASP.NET_MVC_V0._0._2.Models
+{
+    public class Album // We create Album class inside Model, so we can use it later in viewbag(?)
+    {
+        public string Artist { get; set; }
+        public string AlbumName { get; set; }
+    }
+}
