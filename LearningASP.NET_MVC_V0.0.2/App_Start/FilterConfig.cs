7fab9690d7e4275317cf7985def9093120decec0
Hajk <tunczorx@gmail.com>
2018-10-05 11:29:43 +0200

    Add project files.

---
+++ LearningASP.NET_MVC_V0.0.2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LearningASP.NET_MVC_V0._0._2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
